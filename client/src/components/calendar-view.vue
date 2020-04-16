f1bbc61fb886d6d45c9eec9a33f0c2c00b38f4d4
gzwongkk <gzwongkk@gmail.com>
2020-04-16 23:17:22 +0800

    add showing detail on click

--- client/src/components/calendar-view.vue
+++ client/src/components/calendar-view.vue
@@ -1,5 +1,8 @@
 <template>
-    <div id="calendar_view"></div>
+    <div class="view">
+        <div class="calendar" id="calendar_view"></div>
+        <div class="detail" id="detail_view"></div>
+    </div>
 </template>
 
 <script>
@@ -19,6 +22,7 @@
       return {
         // the DOM element
         calendar: null,
+        detail: null,
       }
     },
     watch: {
@@ -28,11 +32,13 @@
     },
     mounted: function () {
       this.initCalendar();
+      this.initDetail();
       this.renderCalendar();
     },
     methods: {
       initCalendar() {
         this.calendar = echarts.init(document.getElementById('calendar_view'));
+        this.calendar.showLoading(this.loadingCalendar);
 
         // An internal function for toolbox item handling periods operation
         let periodOperation = function (params, offset) {
@@ -50,7 +56,7 @@
         // An internal function for formatting tooltip text in heatmap hovers
         let tooltipFormatterHeatmap = function (params) {
           let date = params.value[0];
-          let profit = params.value[1] >= 0? 'profit': 'loss';
+          let profit = params.value[1] >= 0 ? 'profit' : 'loss';
           let value = appendThousandSeparator(Math.abs(params.value[1]));
           return params.marker + date +
             '<br> Net ' + profit + ': ' + value;
@@ -59,7 +65,7 @@
         // An internal function for formatting tooltip text in scatter plot hovers
         let tooltipFormatterScatter = function (params) {
           let date = params.value[0];
-          let profit = params.value[1] >= 0? 'profit': 'loss';
+          let profit = params.value[1] >= 0 ? 'profit' : 'loss';
           let value = appendThousandSeparator(Math.abs(params.value[1]));
           return params.marker + date +
             '<br> Net affiliated ' + profit + ': ' + value;
@@ -75,7 +81,9 @@
                 show: true,
                 title: 'Previous period',
                 icon: 'image://keyboard_arrow_left-black-18dp.svg',
-                onclick: function (params) { periodOperation(params, -1) }
+                onclick: function (params) {
+                  periodOperation(params, -1)
+                }
               },
               myCalendarSwitch: {
                 show: true,
@@ -90,7 +98,9 @@
                 show: true,
                 title: 'Next period',
                 icon: 'image://keyboard_arrow_right-black-18dp.svg',
-                onclick: function (params) { periodOperation(params, +1) }
+                onclick: function (params) {
+                  periodOperation(params, +1)
+                }
               },
             }
           },
@@ -100,24 +110,24 @@
               id: 'src_calendar',
               range: ['2014-01', '2014-03-31'],
               top: 50,
-              bottom: '50%',
+              bottom: '60',
               left: '5%',
               right: '52.5%',
-              itemStyle: { borderWidth: 0.5 },
-              yearLabel: { margin: 7.5 },
-              dayLabel: { show: false },
+              itemStyle: {borderWidth: 0.5},
+              yearLabel: {margin: 7.5},
+              dayLabel: {show: false},
             },
             // right calendar
             {
               id: 'dst_calendar',
               range: ['2014-01', '2014-03-31'],
               top: 50,
-              bottom: '50%',
+              bottom: '60',
               left: '52.5%',
               right: '5%',
-              itemStyle: { borderWidth: 0.5 },
-              yearLabel: { margin: 7.5 },
-              dayLabel: { show: false },
+              itemStyle: {borderWidth: 0.5},
+              yearLabel: {margin: 7.5},
+              dayLabel: {show: false},
             }
           ],
           visualMap: [
@@ -127,7 +137,7 @@
               seriesIndex: 0,
               type: 'continuous',
               orient: 'horizontal',
-              bottom: '43%',
+              bottom: '20',
               left: 'left',
               calculable: true,
               padding: 10,
@@ -137,14 +147,16 @@
               inRange: {
                 color: ['#a6611a', '#dfc27d', '#f5f5f5', '#80cdc1', '#018571'],
               },
-              formatter: (value) => {return appendThousandSeparator(~~(value/1000)) + 'k'},
+              formatter: (value) => {
+                return appendThousandSeparator(~~(value / 1000)) + 'k'
+              },
             },
             {
               id: 'src_vm_scatter',
               seriesIndex: 1,
               type: 'continuous',
               orient: 'horizontal',
-              bottom: '40%',
+              bottom: 'bottom',
               left: '20%',
               padding: 10,
               itemWidth: 10,
@@ -161,7 +173,7 @@
               seriesIndex: 2,
               type: 'continuous',
               orient: 'horizontal',
-              bottom: '43%',
+              bottom: '20',
               right: 'right',
               calculable: true,
               padding: 10,
@@ -171,7 +183,9 @@
               inRange: {
                 color: ['#a6611a', '#dfc27d', '#f5f5f5', '#80cdc1', '#018571'],
               },
-              formatter: (value) => {return appendThousandSeparator(~~(value/1000)) + 'k'},
+              formatter: (value) => {
+                return appendThousandSeparator(~~(value / 1000)) + 'k'
+              },
             },
             {
               id: 'dst_vm_scatter',
@@ -210,7 +224,7 @@
               calendarIndex: 0,
               dimensions: ['date', 'ap_profit', 'rtp_profit'],
               encode: {
-                tooltip: [0,1]
+                tooltip: [0, 1]
               },
               symbolOffset: [0, '-50%'],
               itemStyle: {
@@ -260,15 +274,20 @@
             },
           ]
         });
+
+        this.calendar.on('click', function(event){
+          this.renderDetail(~~(event.seriesIndex/2), event.dataIndex);
+        }, this);
       },
       renderCalendar() {
         if (this.calendarData === undefined || this.calendarData.length === 0) return;
+        this.calendar.showLoading(this.loadingCalendar);
         let src_data = this.calendarData[0]['source'];
         let dst_data = this.calendarData[1]['source'];
 
         // date configuration
-        let date_start  = src_data['date'][0];
-        let date_end    = src_data['date'][src_data['date'].length-1];
+        let date_start = src_data['date'][0];
+        let date_end = src_data['date'][src_data['date'].length - 1];
 
         // min-max configuration for heatmap
         let heatmap_interval_src = (Math.max(...src_data['profit']) + Math.abs(Math.min(...src_data['profit'], 0))) / 2;
@@ -280,8 +299,8 @@
         this.calendar.setOption({
           dataset: this.calendarData,
           title: [
-            { left: '100', text: this.calendarSourceId },
-            { right: '100', text: this.calendarTargetId },
+            {left: '100', text: this.calendarSourceId},
+            {right: '100', text: this.calendarTargetId},
           ],
           calendar: [
             // left calendar
@@ -324,28 +343,76 @@
             {
               id: 'src_series_scatter',
               symbol: function (value) {
-                return value[2]? 'diamond': 'circle';
+                return value[2] ? 'diamond' : 'circle';
               },
               symbolSize: function (value) {
-                return value[1] && Math.abs(value[1]/scatter_max_src*15)+5;
+                return value[1] && Math.abs(value[1] / scatter_max_src * 15) + 5;
               },
             },
             // right calendar
             {
               id: 'dst_series_scatter',
               symbol: function (value) {
-                return value[2]? 'diamond': 'circle';
+                return value[2] ? 'diamond' : 'circle';
               },
               symbolSize: function (value) {
-                return value[1] && Math.abs(value[1]/scatter_max_dst*15)+5;
+                return value[1] && Math.abs(value[1] / scatter_max_dst * 15) + 5;
               },
             },
           ]
         });
+        this.calendar.hideLoading(this.loadingCalendar);
       },
+      initDetail() {
+        this.detail = echarts.init(document.getElementById('detail_view'));
+
+        this.detail.setOption({
+          grid: {},
+          xAxis: {
+            type: 'category',
+            gridIndex: 0,
+            axisTick: { alignWithLabel: true },
+            axisLabel: {
+              interval: 0,
+            }
+          },
+          yAxis: {gridIndex: 0},
+          legend: {},
+          series: {
+            type:'bar',
+            seriesLayoutBy: 'row',
+            label: {show:true}
+          },
+        })
+      },
+      renderDetail(sourceIndex, dataIndex) {
+        const source = this.calendarData[sourceIndex].source;
+        const detailDimensions = ['profit', 'revenue', 'expense', 'ap_profit', 'ap_revenue', 'ap_expense', 'rtp_profit', 'rtp_revenue', 'rtp_expense'];
+        let detailData = [detailDimensions, []];
+        detailDimensions.forEach(d=>detailData[1].push(source[d][dataIndex]));
+        this.detail.setOption({
+          dataset: { source: detailData }
+        })
+      }
     }
   }
 </script>
 
 <style scoped>
+    .view{
+        width: 100%;
+        height: 100%;
+    }
+
+    .calendar{
+        width: 100%;
+        height: 60%;
+        /*border: 1px solid steelblue;*/
+    }
+
+    .detail{
+        width: 100%;
+        height: 40%;
+        /*border: 1px solid forestgreen;*/
+    }
 </style>
